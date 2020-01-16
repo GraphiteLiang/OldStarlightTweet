78ddb3eeb688b2839fb98a7537506f6ca6552ed9
石墨 <34311457+liangyi1210@users.noreply.github.com>
2020-01-16 15:06:00 +0800

    Add files via upload

---
+++ StarlightTweet_Version1.2/src/main/java/com/starlight/data/TweetRepository.java
@@ -0,0 +1,11 @@
+package com.starlight.data;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long>{
+
+	List<Tweet> findByAuthorid(Long authorid);
+}
